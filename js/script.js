00c272709b230ad04aca178d73af897ae15ceed6
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-12-20 13:45:04 +0200

    feat: deleted all

--- js/script.js
+++
@@ -1,5 +0,0 @@
-//========================= MODULES =========================//
-
-//import ('./modules/_burger.js'); // Burger
-import ('./modules/_smoothscroll.js'); // SmoothScroll
-import ('./modules/_works_filter.js'); // WorksFilter
